ad05d80e7f53d5c27f1ed33de97f220cb2ce7154
Aylin Viridiana Resendiz Lopez <168490833+AylinResendiz@users.noreply.github.com>
2024-06-05 18:35:04 -0600

    version 2 (si es mayor que 24 vuelve a pedir la hora)

--- ej11.cpp
+++ ej11.cpp
@@ -3,19 +3,29 @@ using namespace std;
 int main()
 {
     int hora;
-    cout<<"Ingrese la hora ";
-    cin>>hora;
-    if (hora>12 && hora<19)
-	{
-	cout<<"BUENAS TARDES";
-	}
-    else if  (hora<12 && hora>1)
-	{
-	cout<<"BUENOS DIAS";
-	}
-    else if (hora>19 && hora<24)
+    cout<<"Ingrese la hora en formato 24hrs. ";
+    do
     {
-    	cout<<"BUENAS  NOCHES";
-    }
+      cin>>hora;
+      if (hora>24)
+       {
+          cout<<"No es una hora \n"<<"por favor vuelva a ingresar la hora. "<<endl;
+       }   
+      else if (hora>12 && hora<19)
+	    {
+	     cout<<"BUENAS TARDES";
+         break;
+	    }
+       else if  (hora<12 && hora>1)
+	   {
+	      cout<<"BUENOS DIAS";
+           break;
+	   }
+      else if (hora>19 && hora<=24)
+       {
+    	 cout<<"BUENAS  NOCHES";
+         break;
+       } 
+    } while (hora);
 	return 0;
 }
